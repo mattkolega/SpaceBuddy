f80e3c5a9eab9e3d1fbaf830a66be6a8c6b81a3f
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2025-08-26 22:14:26 +1000

    Implement most opcodes

---
+++ src/core/cpu.cpp
@@ -0,0 +1,375 @@
+#include <utility>
+
+#include "cpu.h"
+
+#include <common/bitwise.h>
+
+u16 RegisterPair::get() const { return Bitwise::concatBytes(lo, hi); }
+void RegisterPair::set(u16 newVal) { lo = newVal & 0xFF; hi = newVal >> 8; }
+
+void CPU::pushToStack(u16 value) {
+    sp--;
+    mmu.memWrite(sp, (value >> 8) & 0xFF);
+    sp--;
+    mmu.memWrite(sp, value & 0xFF);
+}
+
+u16 CPU::popFromStack() {
+    u8 lo = mmu.memRead(sp);
+    sp++;
+    u8 hi = mmu.memRead(sp);
+    sp++;
+    return Bitwise::concatBytes(lo, hi);
+}
+
+u8 CPU::getCarry() const { return Bitwise::getBitInByte(f, static_cast<u8>(FlagBits::C)); }
+void CPU::setCarry(bool value) { f = Bitwise::modifyBitInByte(f, static_cast<u8>(FlagBits::C), value); }
+
+u8 CPU::getParity() const { return Bitwise::getBitInByte(f, static_cast<u8>(FlagBits::P)); }
+void CPU::setParity(bool value) { f = Bitwise::modifyBitInByte(f, static_cast<u8>(FlagBits::P), value); }
+
+u8 CPU::getAuxCarry() const { return Bitwise::getBitInByte(f, static_cast<u8>(FlagBits::A)); }
+void CPU::setAuxCarry(bool value) { f = Bitwise::modifyBitInByte(f, static_cast<u8>(FlagBits::A), value); }
+
+u8 CPU::getZero() const { return Bitwise::getBitInByte(f, static_cast<u8>(FlagBits::Z)); }
+void CPU::setZero(bool value) { f = Bitwise::modifyBitInByte(f, static_cast<u8>(FlagBits::Z), value); }
+
+u8 CPU::getSign() const { return Bitwise::getBitInByte(f, static_cast<u8>(FlagBits::S)); }
+void CPU::setSign(bool value) { f = Bitwise::modifyBitInByte(f, static_cast<u8>(FlagBits::S), value); }
+
+/* INSTRUCTIONS */
+
+// Carry bit instructions
+
+// Complements carry flag
+void CPU::cmc() {
+    setCarry(~getCarry());
+}
+
+// Sets carry flag
+void CPU::stc() {
+    setCarry(1);
+}
+
+// Single register instructions
+
+// Increments register
+void CPU::inr(u8& reg) {
+    u8 oldValue = reg;
+    reg++;
+
+    setZero(reg == 0);
+    setSign(Bitwise::isNegative(reg));
+    setParity(Bitwise::checkParity(reg));
+    setAuxCarry(Bitwise::checkHalfCarryAdd(oldValue, 1));
+}
+
+// Decrements register
+void CPU::dcr(u8& reg) {
+    u8 oldValue = reg;
+    reg--;
+
+    setZero(reg == 0);
+    setSign(Bitwise::isNegative(reg));
+    setParity(Bitwise::checkParity(reg));
+    setAuxCarry(Bitwise::checkHalfCarrySub(oldValue, 1));
+}
+
+// Complements accumulator
+void CPU::cma() {
+    a = ~a;
+}
+
+// Decimal adjusts accumulator
+void CPU::daa() {
+    u8 offset { 0 };
+
+    if (getSign() == 0 && (a & 0xF) > 0x9 || getAuxCarry() == 1) {
+        offset |= 0x06;
+        setAuxCarry(1);
+    } else {
+        setAuxCarry(0);
+    }
+
+    if (getSign() == 0 && a > 0x99 || getCarry() == 1) {
+        offset |= 0x60;
+        setCarry(1);
+    } else {
+        setCarry(0);
+    }
+
+    (getSign() == 0) ? a += offset : a -= offset;
+
+    // Set flags
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+}
+
+// Data transfer instructions
+
+// Moves data to register
+void CPU::mov(u8& dst, u8 src) {
+    dst = src;
+}
+
+// Stores accumulator at memory address provided by BC or DE
+void CPU::stax(u16 reg) {
+    mmu.memWrite(reg, a);
+}
+
+// Loads accumulator with memory value at address provided by BC or DE
+void CPU::ldax(u16 reg) {
+    a = mmu.memRead(reg);
+}
+
+// Arithmetic instructions
+
+// Adds register value to accumulator
+void CPU::add(u8 reg) {
+    u8 originalValue = a;
+    a += reg;
+
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+    setAuxCarry(Bitwise::checkHalfCarryAdd(originalValue, reg));
+    setCarry((originalValue + reg) > 0xFF);
+};
+
+// Adds register value + carry to accumulator
+void CPU::adc(u8 reg) {
+    u8 originalValue = a;
+    a += reg + getCarry();
+
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+    setAuxCarry(Bitwise::checkHalfCarryAdd(originalValue, reg, getCarry()));
+    setCarry((originalValue + reg + getCarry()) > 0xFF);
+};
+
+// Subtracts register value from accumulator
+void CPU::sub(u8 reg) {
+    u8 originalValue = a;
+    a -= reg;
+
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+    setAuxCarry(Bitwise::checkHalfCarrySub(originalValue, reg));
+    setCarry((originalValue - reg) < 0);
+};
+
+// Subtracts register value + carry from accumulator
+void CPU::sbb(u8 reg) {
+    u8 originalValue = a;
+    a -= reg - getCarry();
+
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+    setAuxCarry(Bitwise::checkHalfCarrySub(originalValue, reg, getCarry()));
+    setCarry((originalValue - reg - getCarry()) < 0);
+};
+
+// Logical instructions
+
+// ANDs register with accumulator
+void CPU::ana(u8 reg) {
+    a &= reg;
+
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+    setCarry(0);
+}
+
+// XORs register with accumulator
+void CPU::xra(u8 reg) {
+    a ^= reg;
+
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+    setAuxCarry(0);
+    setCarry(0);
+}
+
+// ORs register with accumulator
+void CPU::ora(u8 reg) {
+    a |= reg;
+
+    setZero(a == 0);
+    setSign(Bitwise::isNegative(a));
+    setParity(Bitwise::checkParity(a));
+    setAuxCarry(0);
+    setCarry(0);
+}
+
+// Compares register to accumulator
+void CPU::cmp(u8 reg) {
+    u8 result = a - reg;
+
+    setZero(a == reg);
+    setSign(Bitwise::isNegative(result));
+    setParity(Bitwise::checkParity(result));
+    setAuxCarry(Bitwise::checkHalfCarrySub(a, reg));
+    setCarry(a < reg);
+}
+
+// Rotation instructions
+
+// Rotate accumulator left
+void CPU::rlc() {
+    setCarry(Bitwise::getBitInByte(a, 7)); // Set carry to leftmost bit
+    a = std::rotl(a, 1);
+}
+
+// Rotate accumulator right
+void CPU::rrc() {
+    setCarry(Bitwise::getBitInByte(a, 0)); // Set carry to rightmost bit
+    a = std::rotr(a, 1);
+}
+
+// Rotate accumulator left through carry
+void CPU::ral() {
+    u8 carry = getCarry();
+    setCarry(Bitwise::getBitInByte(a, 7)); // Set carry flag to leftmost bit
+    u8 rotated = std::rotl(a, 1);
+    a = Bitwise::modifyBitInByte(rotated, 0, carry);
+}
+
+// Rotate accumulator right through carry
+void CPU::rar() {
+    u8 carry = getCarry();
+    setCarry(Bitwise::getBitInByte(a, 0));  // Set carry flag to rightmost bit
+    u8 rotated = std::rotr(a, 1);
+    a = Bitwise::modifyBitInByte(rotated, 7, carry);
+}
+
+// Register pair instructions
+
+// Pushes register pair to stack
+void CPU::push(RegisterPair reg) {
+    pushToStack(reg.get());
+}
+
+// Pops register pair from stack
+void CPU::pop(RegisterPair &reg) {
+    reg = popFromStack();
+}
+
+// Adds register pair to HL
+void CPU::dad(u16 reg){
+    u8 originalValue = hl.get();
+    hl.set(hl.get() + reg);
+    setCarry((originalValue + reg) > 0xFFFF);
+}
+
+// Increments register pair
+void inx(RegisterPair& reg) {
+    reg.set(reg.get() + 1);
+}
+
+// Increments word
+void inx(u16& reg) {
+    reg++;
+}
+
+// Decrements register pair
+void CPU::dcx(RegisterPair& reg){
+    reg.set(reg.get() - 1);
+}
+
+// Decrements word
+void CPU::dcx(u16& reg){
+    reg--;
+}
+
+// Swaps DE and HL values
+void CPU::xchg(){
+    std::swap(d, h);
+    std::swap(e, l);
+}
+
+// Swaps HL and top word on stack
+void CPU::xthl(){
+    u8 lo = mmu.memRead(sp);
+    mmu.memWrite(sp, l);
+    u8 hi = mmu.memRead(sp+1);
+    mmu.memWrite(sp+1, h);
+    hl.set(Bitwise::concatBytes(lo, hi));
+}
+
+// Sets SP to HL
+void CPU::sphl(){
+    sp = hl.get();
+}
+
+// Immediate instructions
+
+// Loads immediate value into register pair
+void CPU::lxi(RegisterPair& reg, u16 immediate) {
+    reg.set(immediate);
+}
+
+// Loads immediate value into word-sized register
+void CPU::lxi(u16& reg, u16 immediate) {
+    reg = immediate;
+}
+
+// Direct addressing instructions
+
+// Stores accumulator in memory
+void CPU::sta(u16 address) {
+    mmu.memWrite(address, a);
+}
+
+// Loads accumulator from memory
+void CPU::lda(u16 address) {
+    a = mmu.memRead(address);
+}
+
+// Stores HL in memory
+void CPU::shld(u16 address) {
+    mmu.memWrite(address, hl.get());
+}
+
+// Loads HL from memory
+void CPU::lhld(u16 address) {
+    hl.set(mmu.memRead(address));
+}
+
+// Jump instructions
+
+// Jumps to address in HL
+void CPU::pchl() {
+    pc = hl.get();
+}
+
+// Jumps to address
+void CPU::jmp(u16 address, bool condition) {
+    if (condition) pc = address;
+}
+
+// Call subroutine instructions
+
+// Pushes PC to stack and jumps to address
+void CPU::call(u16 address, bool condition) {
+    if (condition) {
+        pushToStack(pc);
+        pc = address;
+    }
+}
+
+// Pops PC from stack
+void CPU::ret(u16 address, bool condition) {
+    if (condition) pc = popFromStack();
+}
+
+// Restarts
+void CPU::rst(u8 data) {
+    pushToStack(pc);
+    pc = data * 8;
+}
---
+++ src/core/cpu.h
@@ -0,0 +1,153 @@
+#pragma once
+
+#include <string_view>
+
+#include <common/bitwise.h>
+#include <common/types.h>
+
+#include "mmu.h"
+
+enum class FlagBits : u8 {
+    C = 0, // Carry
+    P = 2, // Parity
+    A = 4, // Aux Carry
+    Z = 6, // Zero
+    S = 7, // Sign
+};
+
+class RegisterPair {
+public:
+    u8 hi {};
+    u8 lo {};
+
+    RegisterPair() = default;
+    RegisterPair(u16 value) : hi(value >> 8), lo(value & 0xFF) {};
+
+    u16 get() const;
+    void set(u16 value);
+};
+
+struct opcodeInfo {
+    std::string_view mnemonic;
+    u8 cycles;
+};
+
+class CPU {
+public:
+    CPU() = delete;
+
+    // Executes a single opcode.
+    void step();
+private:
+    MMU &mmu;
+
+    RegisterPair psw {};
+    RegisterPair bc  {};
+    RegisterPair de  {};
+    RegisterPair hl  {};
+
+    u16 pc {};
+    u16 sp {};
+
+    u8& a = psw.hi;
+    u8& f = psw.lo;
+    u8& b = bc.hi;
+    u8& c = bc.lo;
+    u8& d = de.hi;
+    u8& e = de.lo;
+    u8& h = hl.hi;
+    u8& l = hl.lo;
+    u8& m = mmu.get(hl.get());
+
+    void pushToStack(u16 value);
+    u16 popFromStack();
+
+    u8 getCarry() const;
+    void setCarry(bool value);
+    u8 getParity() const;
+    void setParity(bool value);
+    u8 getAuxCarry() const;
+    void setAuxCarry(bool value);
+    u8 getZero() const;
+    void setZero(bool value);
+    u8 getSign() const;
+    void setSign(bool value);
+
+    void execute();
+
+    /* INSTRUCTIONS */
+
+    // Carry bit instructions
+    void cmc();
+    void stc();
+
+    // Single register instructions
+    void inr(u8& reg);
+    void dcr(u8& reg);
+    void cma();
+    void daa();
+
+    // Data transfer instructions
+    void mov(u8& dst, u8 src);
+    void stax(u16 reg);
+    void ldax(u16 reg);
+
+    // Arithmetic instructions
+    void add(u8 reg);
+    void adc(u8 reg);
+    void sub(u8 reg);
+    void sbb(u8 reg);
+
+    // Logical instructions
+    void ana(u8 reg);
+    void xra(u8 reg);
+    void ora(u8 reg);
+    void cmp(u8 reg);
+
+    // Rotation instructions
+    void rlc();
+    void rrc();
+    void ral();
+    void rar();
+
+    // Register pair instructions
+    void push(RegisterPair reg);
+    void pop(RegisterPair& reg);
+    void dad(u16 reg);
+    void inx(RegisterPair& reg);
+    void inx(u16& reg);
+    void dcx(RegisterPair& reg);
+    void dcx(u16& reg);
+    void xchg();
+    void xthl();
+    void sphl();
+
+    // Immediate instructions
+    void lxi(RegisterPair& reg, u16 immediate);
+    void lxi(u16& reg, u16 immediate);
+
+    // Direct addressing instructions
+    void sta(u16 address);
+    void lda(u16 address);
+    void shld(u16 address);
+    void lhld(u16 address);
+
+    // Jump instructions
+    void pchl();
+    void jmp(u16 address, bool condition);
+
+    // Call subroutine instructions
+    void call(u16 address, bool condition);
+    void ret(u16 address, bool condition);
+    void rst(u8 data);
+
+    // Interrupt instructions
+    void ei();
+    void di();
+
+    // Input/output instructions
+    void in();
+    void out();
+
+    void halt();
+};
