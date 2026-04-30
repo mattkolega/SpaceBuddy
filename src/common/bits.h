b1d12fdaa7a499074d6980778f05f2dea38c5a36
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2026-04-30 19:23:33 +1000

    Rename namespace to all lowercase

--- src/common/bits.h
+++ src/common/bits.h
@@ -4,64 +4,64 @@
 
 #include "types.h"
 
-namespace Bits {
+namespace bits {
     // Checks if half carry occurred in 8-bit addition
-    inline bool checkHalfCarryAdd(u8 operand1, u8 operand2, u8 carry = 0) {
+    constexpr bool checkHalfCarryAdd(u8 operand1, u8 operand2, u8 carry = 0) {
         return ((operand1 & 0xF) + (operand2 & 0xF) + (carry & 0b1)) > 0xF;
     }
 
     // Checks if half carry occurred in 16-bit addition
-    inline bool checkHalfCarryAdd(u16 operand1, u16 operand2, u8 carry = 0) {
+    constexpr bool checkHalfCarryAdd(u16 operand1, u16 operand2, u8 carry = 0) {
         return ((operand1 & 0xFFF) + (operand2 & 0xFFF) + (carry & 0b1)) > 0xFFF;
     }
 
     // Checks if half carry occurred in 8-bit subtraction
-    inline bool checkHalfCarrySub(u8 operand1, u8 operand2, u8 carry = 0) {
+    constexpr bool checkHalfCarrySub(u8 operand1, u8 operand2, u8 carry = 0) {
         return (operand1 & 0xF) < ((operand2 & 0xF) + (carry & 0b1));
     }
 
     // Checks if half carry occurred in 16-bit subtraction
-    inline bool checkHalfCarrySub(u16 operand1, u16 operand2, u8 carry = 0) {
+    constexpr bool checkHalfCarrySub(u16 operand1, u16 operand2, u8 carry = 0) {
         return (operand1 & 0xFFF) < ((operand2 & 0xFFF) + (carry & 0b1));
     }
 
     // Gets n-th bit from an 8-bit value
-    inline u8 getBitInByte(u8 value, u8 n) {
+    constexpr u8 getBitInByte(u8 value, u8 n) {
         return (value >> n) & 0b1;
     }
 
     // Modifies a specific bit in a 8-bit value
-    inline u8 modifyBitInByte(u8 value, u8 n, u8 newVal) {
+    constexpr u8 modifyBitInByte(u8 value, u8 n, u8 newVal) {
         return (value & ~(1 << n)) | ((newVal & 1) << n);
     }
 
     // Grabs the first 4-bits from a byte
-    inline u8 getFirstNibble(u8 value) {
+    constexpr u8 getFirstNibble(u8 value) {
         return (value >> 4) & 0xF;
     }
 
     // Grabs the second 4-bits from a byte
-    inline u8 getSecondNibble(u8 value) {
+    constexpr u8 getSecondNibble(u8 value) {
         return value & 0xF;
     }
 
     // Swaps the upper and lower nibbles of a byte
-    inline u8 swapNibbles(u8 value) {
+    constexpr u8 swapNibbles(u8 value) {
         return (value << 4) | (value >> 4);
     }
 
     // Joins two bytes together to make a 16-bit value
-    inline u16 concatBytes(u8 low, u8 hi) {
+    constexpr u16 concatBytes(u8 low, u8 hi) {
         return (hi << 8) | low;
     }
 
     // Checks the sign of a byte. True if 7th bit set, false if not
-    inline bool isNegative(u8 value) {
+    constexpr bool isNegative(u8 value) {
         return getBitInByte(value, 7);
     }
 
     // Checks parity of a byte. True if even, false if odd
-    inline bool checkParity(u8 value) {
+    constexpr bool checkParity(u8 value) {
         return std::popcount(value) % 2 == 0;
     }
 }
--- src/common/log.h
+++ src/common/log.h
@@ -7,7 +7,7 @@
 #include <fmt/color.h>
 #include <fmt/core.h>
 
-namespace Log {
+namespace log {
     namespace internal {
         // Get current system time as a string
         inline std::string getCurrentTime() {
@@ -33,32 +33,32 @@ namespace Log {
     // Logs a message which is useful for debugging and isn't relevant to the user
     template<typename... Args>
     void debug(fmt::format_string<Args...> formatString, Args&&... args) {
-        Log::internal::print("DEBUG", fg(fmt::color::light_golden_rod_yellow), formatString, std::forward<Args>(args)...);
+        log::internal::print("DEBUG", fg(fmt::color::light_golden_rod_yellow), formatString, std::forward<Args>(args)...);
     }
 
     // Logs a general message which informs the user about the state of the program
     template<typename... Args>
     void info(fmt::format_string<Args...> formatString, Args&&... args) {
-        Log::internal::print("INFO", fg(fmt::color::powder_blue), formatString, std::forward<Args>(args)...);
+        log::internal::print("INFO", fg(fmt::color::powder_blue), formatString, std::forward<Args>(args)...);
     }
 
     // Logs a warning message which indicates that something may have gone wrong but isn't necessarily an error
     template<typename... Args>
     void warn(fmt::format_string<Args...> formatString, Args&&... args) {
-        Log::internal::print("WARN", fg(fmt::color::orange), formatString, std::forward<Args>(args)...);
+        log::internal::print("WARN", fg(fmt::color::orange), formatString, std::forward<Args>(args)...);
     }
 
     // Logs an error message which indicates that something has gone wrong during program execution
     // May or may not lead to program exit
     template<typename... Args>
     void err(fmt::format_string<Args...> formatString, Args&&... args) {
-        Log::internal::print("ERROR", fg(fmt::color::orange_red), formatString, std::forward<Args>(args)...);
+        log::internal::print("ERROR", fg(fmt::color::orange_red), formatString, std::forward<Args>(args)...);
     }
 
     // Logs an error message which indicates that something has gone massively wrong during program execution
     // Should lead to program exit
     template<typename... Args>
     void fatal(fmt::format_string<Args...> formatString, Args&&... args) {
-        Log::internal::print("FATAL", fg(fmt::color::crimson), formatString, std::forward<Args>(args)...);
+        log::internal::print("FATAL", fg(fmt::color::crimson), formatString, std::forward<Args>(args)...);
     }
 }
