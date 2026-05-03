50eb89244994e0e6fb55ac82df511f1357048c1d
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2026-05-03 13:45:24 +1000

    Improve error logging

--- src/common/fs.h
+++ src/common/fs.h
@@ -1,28 +1,40 @@
 #pragma once
 
+#include <cerrno>
+#include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <optional>
 #include <vector>
 
+#include "log.h"
 #include "types.h"
 
 namespace fs {
     inline std::optional<std::vector<u8>> loadFileIntoBuffer(const std::filesystem::path& filepath) {
         std::ifstream file {filepath, std::ios::binary};
-        if (!file) return std::nullopt;
+        if (!file) {
+            log::fatal("Failed to open file: `{}` Error: {}", filepath, std::strerror(errno));
+            return std::nullopt;
+        }
 
         // Get filesize
         file.seekg(0, std::ios::end);
         auto fileSize = file.tellg();
-        if (fileSize < 0) return std::nullopt;
+        if (fileSize < 0) {
+            log::fatal("Failed to open file: `{}` Error: Invalid file size", filepath);
+            return std::nullopt;
+        }
         file.seekg(0, std::ios::beg);
 
         std::vector<u8> buffer(fileSize);
 
         // Copy file data to buffer
         file.read(reinterpret_cast<char*>(buffer.data()), fileSize);
-        if (!file) return std::nullopt;
+        if (!file) {
+            log::fatal("Failed to open file: `{}` Error: {}", filepath, std::strerror(errno));
+            return std::nullopt;
+        }
 
         return buffer;
     }
